28ff1f01b59758bb909ea34ac266c1a672ce4c29
Mahadev1729 <devcampus17@gmail.com>
2026-03-14 20:29:13 +0530

    implement login functionality with form handling and state management

--- employeems/src/Components/Login.jsx
+++ employeems/src/Components/Login.jsx
@@ -1,12 +1,24 @@
-﻿import "./style.css";
+﻿import { useState } from "react";
+import "./style.css";
+import axios from axios
 
 const Login = () => {
+    const [values,setValues]=useState({
+        email:'',
+        password:''
+    })
+    const handleSubmit=(event)=>{
+       event.preventDefault()
+       axios.post('http://localhost:3000/auth/adminlogin')
+       .then (result=>console.log(result))
+       .catch(err=>console.log(err))
+    }
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
       <div className="p-4 rounded w-25 border loginForm">
         <h2 className="text-center">Login Page</h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label>
               <strong>Email:</strong>
@@ -17,7 +29,8 @@ const Login = () => {
               name="email"
               autoComplete="off"
               placeholder="Enter email here"
-              className="form-control"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+              className="form-control rounded-0"
             />
           </div>
 
@@ -31,11 +44,17 @@ const Login = () => {
               name="password"
               autoComplete="off"
               placeholder="Enter password here"
+              onChange={(e) => setValues({ ...values, password: e.target.value })}
               className="form-control"
             />
           </div>
-
-          <button className="btn btn-success w-100">Login</button>
+          <button className="btn btn-success w-100">Log In</button>
+          <div className="mb-3">
+            <input type="checkbox" name="tick" id="tick" />
+            <label htmlFor="">
+              <strong>You are agree with terms and conditions?</strong>
+            </label>
+          </div>
         </form>
       </div>
     </div>
