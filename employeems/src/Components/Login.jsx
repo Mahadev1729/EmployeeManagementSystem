e26449ffb245716d309e75bce3b98642cfbd7dff
Mahadev1729 <devcampus17@gmail.com>
2026-03-14 19:22:27 +0530

    add background image and update Login component styling

--- employeems/src/Components/Login.jsx
+++ employeems/src/Components/Login.jsx
@@ -1,38 +1,45 @@
-﻿
-const Login=()=>{
-    return (
-      <div className="d-flex jsutify-content-center align-items-center vh-100 loginPage">
-        <div className="p-3 rounded w-25 border loginForm">
-          <h2>Login Page</h2>
-          <form>
-            <div>
-              <label htmlFor="email">
-                <strong>Email:</strong>
-              </label>
-              <input
-                type="email"
-                name="email"
-                autoComplete="off"
-                placeholder="Enter emial here"
-                className="form-control rounded-0"
-              />
-            </div>
-            <div>
-              <label htmlFor="password">
-                <strong>Password:</strong>
-              </label>
-              <input
-                type="passwprd"
-                name="password"
-                autoComplete="off"
-                placeholder="Enter password here"
-                className="form-control rounded-0"
-              />
-            </div>
-            <button className="btn btn-success w-100 rounded-0">Submit</button>
-          </form>
-        </div>
+﻿import "./style.css";
+
+const Login = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="p-4 rounded w-25 border loginForm">
+        <h2 className="text-center">Login Page</h2>
+
+        <form>
+          <div className="mb-3">
+            <label>
+              <strong>Email:</strong>
+            </label>
+
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter email here"
+              className="form-control"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>
+              <strong>Password:</strong>
+            </label>
+
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              placeholder="Enter password here"
+              className="form-control"
+            />
+          </div>
+
+          <button className="btn btn-success w-100">Login</button>
+        </form>
       </div>
-    );
-}
+    </div>
+  );
+};
+
 export default Login;
